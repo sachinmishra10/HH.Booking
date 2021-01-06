1c789272f1d6935c74c55ffff6e549c149b3d7e3
sachin mishra <sachin.mishra10@gmail.com>
2021-01-06 19:58:19 +0530

    Test merge functionlity

--- HH.Tool/Program.cs
+++ HH.Tool/Program.cs
@@ -7,6 +7,7 @@ namespace HH.Tool
         static void Main(string[] args)
         {
             //Just check Fetch Functionlity of Git
+            //Merge from local
             Console.WriteLine("Hello World!!!");
         }
     }
